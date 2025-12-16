7efe0aa00a5eec67c573e0950403112d8ddb0a83
sudhanshu <risinginfranoida@gmail.com>
2025-12-17 01:57:19 +0530

    Add

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,8 +4,8 @@ import Stripe from "stripe";
 //   throw new Error("STRIPE_SECRET_KEY is not defined");
 // }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-03-31.basil",
-});
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: "2025-03-31.basil",
+// });
 
 export default stripe;
